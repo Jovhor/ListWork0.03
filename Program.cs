9bd77df0d17d25a03b4d56e8e721d0b2b236adf6
Jovhor <dr.jovhor@mail.ru>
2024-09-24 19:43:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListWork0._03
+{
+    class Program
+    {
+        static void Main()
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            while (true)
+            {
+                int number;
+                bool isOk = false;
+                do
+                {
+                    Console.Write("Введите целое число: ");
+                    string userInput = Console.ReadLine();
+
+                    if (int.TryParse(userInput, out number))
+                    {
+                        isOk = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Только числа, братан.");
+                    }
+                } while (!isOk);
+
+                if (isOk)
+                {
+                    if (set.Add(number)) Console.WriteLine($"Что-то новенькое. Молодец!");
+                    else Console.WriteLine($"Баян. Давай по-новой.");
+                }
+            }
+        }
+    }
+}
